672d1c4507cf1d9ca7196e28f2191deecd3ede3e
Lkhant6916 <155945752+Lkhant6916@users.noreply.github.com>
2024-02-09 15:47:50 +0800

    Update currency.js

--- scripts/currency.js
+++ scripts/currency.js
@@ -5,15 +5,20 @@ function calculate() {
     USD: 0.74,
     MYR: 3.51,
   };
-  var input = parseFloat(document.getElementById("school-fees").value);
+  var input = document.getElementById("school-fees");
   var selectedValue = document.querySelector('input[name="currency"]:checked');
   var calculatedValue;
   var answer = "";
-  if (selectedValue != null) {
-    calculatedValue = input * currencyRates[selectedValue.value];
-    answer = calculatedValue + " " + selectedValue.value;
+  if (!isNaN(input.value) && input.value > 0) {
+    input = parseFloat(document.getElementById("school-fees").value);
+    if (selectedValue != null) {
+      calculatedValue = input * currencyRates[selectedValue.value];
+      answer = calculatedValue + " " + selectedValue.value;
+    } else {
+      answer = "Please select currency.";
+    }
   } else {
-    answer = "Please select currency.";
+    answer = "Please type a valid number";
   }
   document.getElementById("ans").innerHTML = answer;
 }
